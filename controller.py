1ce0fbeaa6be81aa8f98d7dfeae715ca1ef221e9
Probst <mprobst6@gatech.edu>
2021-11-02 17:37:07 -0400

    centering button works

--- controller.py
+++ controller.py
@@ -36,10 +36,10 @@ horizontal_right_joystick = 2
 vertical_right_joystick = 3
 
 # Hat Mapping
-right = (0,1)
-left = (0,-1)
-up = (1,1)
-down = (1,-1)
+right = (0,1); left = (0,-1); up = (1,1); down = (1,-1)
+
+# Center commands
+translation_center = 12.5; rotation_center = 6
 
 # Jump Distance
 # jump_by =  # change in um
@@ -53,6 +53,13 @@ kcube_ya = KCubeDCServo(27258530)
 kcube_z = KCubeDCServo(27258551)
 kcube_y = KCubeDCServo(27258581)
 kcube_x = KCubeDCServo(27258584)
+
+# Make lists to operate on sets of motors
+all_motors = [kcube_x,kcube_y,kcube_z,kcube_yi,kcube_ya]
+translation = [kcube_x,kcube_y,kcube_z]
+rotation = [kcube_yi,kcube_ya]
+left_group = [kcube_x,kcube_y]
+right_group = [kcube_yi,kcube_ya,kcube_z]
 
 
 kcube_x.create(); kcube_y.create(); kcube_z.create(); kcube_yi.create(); kcube_ya.create()
@@ -98,11 +105,15 @@ class PS4Controller(object):
                 if event.type == pygame.JOYBUTTONDOWN:
                     self.button_data[event.button] = True    
                     if self.button_data[options_button] == True:
-                        kcube_x.stop_immediate()
-                        kcube_y.stop_immediate()
-                        kcube_z.stop_immediate()
-                        kcube_yi.stop_immediate()
-                        kcube_ya.stop_immediate()
+                        for motor in all_motors:
+                            motor.stop_immediate()
+                    elif self.button_data[share_button] == True:
+                        for motor in all_motors:
+                            motor.stop()
+                        for motor in rotation:
+                            motor.move_absolute(rotation_center)
+                        for motor in translation:
+                            motor.move_absolute(translation_center)
 
 
                 elif event.type == pygame.JOYBUTTONUP:
@@ -140,55 +151,55 @@ class PS4Controller(object):
 # First two if statements set the velocity based on whether the button has been pressed or not
                     if event.axis == horizontal_left_joystick or event.axis == vertical_left_joystick: # controls the left joystick
 # negative signs necessary to align the controller directions with the joystick directions
-                        left_velocity = int(-round(event.value,2)*max_speed)
+                        left_velocity_du = int(-round(event.value,2)*max_speed)
                         if self.button_data[left_joystick_button] == False:
-                            left_velocity = 0.2*left_velocity  # 20% of speed without button push
+                            left_velocity_du = 0.2*left_velocity_du # 20% of speed without button push
                     if event.axis == horizontal_right_joystick or event.axis == vertical_right_joystick: # controls the right joystick
-                        right_velocity = int(-round(event.value,2)*max_speed)
+                        right_velocity_du = int(-round(event.value,2)*max_speed)
                         if self.button_data[right_joystick_button] == False:
-                            right_velocity = 0.2*right_velocity # 20% of speed without button push
+                            right_velocity_du = 0.2*right_velocity_du # 20% of speed without button push
                             
 # Controls motion in the x direction
                     self.axis_data[event.axis] = round(event.value,2)
                     if event.axis == horizontal_left_joystick:
-                        if left_velocity == 0: # calls stop when the velocity is zero, buggy otherwise
+                        if left_velocity_du == 0: # calls stop when the velocity is zero, buggy otherwise
                             kcube_x.stop()
                         else:
-                            kcube_x.velocity(left_velocity)
-                            # print("x velocity: {}".format(left_velocity))
+                            kcube_x.velocity(left_velocity_du)
+                            # print("x velocity: {}".format(left_velocity_du))
 # Controls motion in the y direction
                     elif event.axis == vertical_left_joystick:
-                        if left_velocity == 0: # calls stop when the velocity is zero
+                        if left_velocity_du == 0: # calls stop when the velocity is zero
                             kcube_y.stop()
                         else:
-                            kcube_y.velocity(left_velocity)
-                            # print("y velocity {}".format(left_velocity))
+                            kcube_y.velocity(left_velocity_du)
+                            # print("y velocity {}".format(left_velocity_du))
 # When the trigger is held, control the yi
                     elif event.axis == horizontal_right_joystick:
                         if self.button_data[right_front_trigger] == True:
-                            if right_velocity == 0:
+                            if right_velocity_du == 0:
                                 kcube_yi.stop()
                             else:
-                                kcube_yi.velocity(right_velocity)
-                                print("yi velocity: {}".format(right_velocity))
+                                kcube_yi.velocity(right_velocity_du)
+                                print("yi velocity: {}".format(right_velocity_du))
 # right/left on the left joystick does nothing when the right trigger is not held because only up and down controls the z
                         else:
                             print("left right does nothing right now")
 # When the trigger is held, control ya
                     elif event.axis == vertical_right_joystick:
                         if self.button_data[right_front_trigger] == True:
-                            if right_velocity == 0:
+                            if right_velocity_du == 0:
                                 kcube_ya.stop()
                             else:
-                                kcube_ya.velocity(right_velocity)
-                                print("ya velocity: {}".format(right_velocity))
+                                kcube_ya.velocity(right_velocity_du)
+                                print("ya velocity: {}".format(right_velocity_du))
 # else control z
                         else: # control z axis when the button is not held
-                            if right_velocity == 0:
+                            if right_velocity_du == 0:
                                 kcube_z.stop()
                             else:
-                                kcube_z.velocity(right_velocity)
-                                print("z velocity: {}".format(right_velocity))
+                                kcube_z.velocity(right_velocity_du)
+                                print("z velocity: {}".format(right_velocity_du))
 if __name__ == "__main__":
     ps4 = PS4Controller()
     ps4.init()
